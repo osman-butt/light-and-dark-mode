15a3a6909313100458b3d38565cbc765c83a96ec
osman-butt <butt.osman@gmail.com>
2023-03-15 22:12:10 +0100

    Part 1+2

--- app.js
+++ app.js
@@ -3,11 +3,33 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
-    //event listener for select on HTML
-    document.querySelector("#select-color-mode").addEventListener("change", modeSelected);
+  //event listener for select on HTML
+  document
+    .querySelector("#select-color-mode")
+    .addEventListener("change", modeSelected);
 }
 
 // modeSelected called when #select-color-mode changes value (the user select color mode)
 function modeSelected() {
-    console.log("New color mode selected");
+  const selectedColorMode = this.value;
+  changeMode(selectedColorMode);
+  console.log("New color mode selected");
+}
+
+function changeMode(mode) {
+  console.log("changeMode");
+  resetColorMode();
+  if (mode == "dark") {
+    document.querySelector("body").classList.add("dark-mode");
+  } else if (mode == "green") {
+    document.querySelector("body").classList.add("green-mode");
+  } else if (mode == "yellow") {
+    document.querySelector("body").classList.add("yellow-mode");
+  }
+}
+
+function resetColorMode() {
+  document.querySelector("body").classList.remove("dark-mode");
+  document.querySelector("body").classList.remove("green-mode");
+  document.querySelector("body").classList.remove("yellow-mode");
 }
